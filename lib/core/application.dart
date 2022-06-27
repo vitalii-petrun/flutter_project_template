8feafddcc79caa660a6c722d4117f3ae36d4722c
Vladimir <vovaklh20012@gmail.com>
2022-06-27 16:43:06 +0300

    setup localization

--- lib/core/application.dart
+++ lib/core/application.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:project_template/l10n/l10n.dart';
 import 'package:project_template/presentation/pages/home_page.dart';
+import 'package:flutter_localizations/flutter_localizations.dart';
 
 class Application extends StatelessWidget {
   const Application({Key? key}) : super(key: key);
@@ -11,6 +13,12 @@ class Application extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
+      localizationsDelegates: const [
+        GlobalMaterialLocalizations.delegate,
+        GlobalWidgetsLocalizations.delegate,
+        GlobalCupertinoLocalizations.delegate,
+      ],
+      supportedLocales: L10n.all,
       home: const HomePage(title: 'Flutter Demo Home Page'),
     );
   }
