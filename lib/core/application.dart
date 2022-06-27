dec1a5d3c1ed58fe6e074cde4917a0d3f58bdab1
Vladimir <vovaklh20012@gmail.com>
2022-06-27 16:30:13 +0300

    create project structure

---
+++ lib/core/application.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+import 'package:project_template/presentation/pages/home_page.dart';
+
+class Application extends StatelessWidget {
+  const Application({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: const HomePage(title: 'Flutter Demo Home Page'),
+    );
+  }
+}
